ec80440b5f75562f0c76b8a7a8ad9cbc4809f6f7
RomainBoudet <romainoclock@gmail.com>
2022-03-08 00:50:18 +0100

    search bar

--- app/controller/mainController.js
+++ app/controller/mainController.js
@@ -128,6 +128,72 @@ const mainController = {
 
     },
 
+    search: async (req, res) => {
+
+        try {
+
+            const pokemon = req.body.search;
+
+            console.log("pokemon ==>> ", pokemon);
+            // on vérifit que l'utilisateur nous a bien donné un nom de pokemon !
+
+            // je récupére tous les nom de pokemon et je tcheke avec un includes
+
+            // si pas dans l'includes alors le relance la homePage avec un toast !
+
+            // si le nom existe, on renvoie le match 
+
+
+            // vérification du type et rendu en cas d'insertion d'un mot inconnu !
+            const allTypes = await datamapper.getAllTypes();
+
+            const toastDate = await formatToast();
+
+            if (!allTypes.includes(type)) {
+                // renvoie de la vue typeList avec un toast
+                const toastMessage = `⚠️ Ce type de pokemon (${type}) n'existe pas ! ❌ `;
+
+                const typePokemon = await datamapper.getAllList();
+                const allType = typePokemon[0].colorByType;
+
+                console.log(`⚠️ Ce type de pokemon (${type}) n'existe pas ! ❌❌❌`);
+
+
+                return res.status(200).render('typeList', {
+                    allType,
+                    toastDate,
+                    toastMessage
+                })
+            }
+
+            // si le type existe mais ne posséde aucun pokemon :
+            const pokemons = await datamapper.getPokemonsByType(type);
+
+            if (pokemons === null) {
+
+                const typePokemon = await datamapper.getAllList();
+                const allType = typePokemon[0].colorByType;
+                const toastMessage = `Ce type existe bien, mais dans cette BDD, aucun pokemon n'existe avec le type ${type} !  ❌ `;
+                console.log(`Ce type existe bien, mais dans cette BDD, aucun pokemon n'existe avec le type ${type} !  ❌❌❌ `);
+                return res.status(200).render('typeList', {
+                    allType,
+                    toastDate,
+                    toastMessage
+                })
+            }
+
+
+            return res.status(200).render('typeDetail', {
+                pokemons
+            });
+
+        } catch (error) {
+            console.log("Erreur dans le mainController, dans la méthode typeList : ", error);
+            res.status(500).end();
+        }
+
+    },
+
 
 };
 
--- app/router/router.js
+++ app/router/router.js
@@ -8,6 +8,7 @@ router.get('/', mainController.homepage);
 router.get('/pokemon/:id(\\d+)', mainController.pokemonPage);
 router.get('/type', mainController.typeList);
 router.get('/type/:type', mainController.typeDetail);
+router.post('/pokemon/search', mainController.search);
 
 
 module.exports = router;
