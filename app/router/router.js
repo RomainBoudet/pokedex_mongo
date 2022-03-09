6559f99c9d2f7c429d19f6fd5e31bde85129a437
RomainBoudet <romainoclock@gmail.com>
2022-03-09 10:48:48 +0100

    helmet, CSP  and 404

--- app/router/router.js
+++ app/router/router.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const {clean, cleanQuery} = require('../middleware/sanitizer');
+const {
+    clean,
+    cleanQuery
+} = require('../middleware/sanitizer');
 
-const mainController  = require('../controller/mainController');
+const mainController = require('../controller/mainController');
 
 
 router.get('/', cleanQuery, mainController.homepage);
@@ -12,6 +15,12 @@ router.get('/type', mainController.typeList);
 router.get('/type/:type', mainController.typeDetail);
 router.post('/pokemon/search', clean, mainController.search);
 
+/**
+ * Redirection vers l'acceuil en cas de "404".
+ */
+router.use((req, res) => {
+    res.status(404).redirect('/');
+})
+
 
 module.exports = router;
-
--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 require('dotenv').config();
 const chalk = require('chalk'); // version 4 sinon need ESM
 const express = require('express');
+const helmet = require('helmet');
+const crypto =require('crypto');
 const app = express ();
 
 const router = require ('./app/router/router');
@@ -22,13 +24,40 @@ const port = process.env.PORT;
 // Si je veux récupérer l'ip dérriere le proxy NGINX
 app.set('trust proxy', true); // L'adresse ip du client est intéerprétés comme celle la plus a gauche dans l'en tête X-Forwarded-* https://expressjs.com/fr/guide/behind-proxies.html 
 
-// Petite défense contre les xss par défault
-app.use((req, res, next) => {
-    res.setHeader("X-XSS-Protection", "1; mode=block");
-    next();
+app.use(helmet());
+
+// Config for sub-resources integrity 
+app.use((_, res, next) => {
+  res.locals.nonce = crypto.randomBytes(16).toString("hex");
+  next();
 });
-// Personne ne sait que l'app tourne sous express dans les en tête http !
-app.set('x-powered-by', false);
+
+// CSP configuration and headers security
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: [`'self'`,], 
+      "script-src": [(_, res) => `'nonce-${res.locals.nonce}'`],
+      "img-src": [`'self'` ], // "data:" nonce ne fonctionne pas avec les img car pas d'attribut nonce 
+      
+      "style-src": [ `'self'`,"'unsafe-inline'", 'https://fonts.googleapis.com/css?family=Bree+Serif&display=swap'], //
+      "base-uri": ["'none'"],
+      "object-src":["'none'"],
+    
+      upgradeInsecureRequests: [] 
+    }
+  }));
+
+// quelques configuration de headers...
+app.use((req, res, next) => {
+    res.setHeader(
+      "Permissions-Policy",
+      "geolocation=(), fullscreen=(), autoplay=(), camera=(), display-capture=(), document-domain=(), fullscreen=(), magnetometer=(), microphone=(), midi=(), payment=(), picture-in-picture=(), publickey-credentials-get=(), sync-xhr=(), usb=(), screen-wake-lock=(), xr-spatial-tracking=()"
+    );
+      res.setHeader("X-XSS-Protection", "1; mode=block");
+      next();
+    });
+  
+  app.set('x-powered-by', false);
 
 app.locals.pageTitle = `PokeDex`;
 app.locals.pageDescription = `Attrapez les tous !`;
